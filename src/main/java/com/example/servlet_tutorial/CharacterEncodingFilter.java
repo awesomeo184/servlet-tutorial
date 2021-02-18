cf2cacbe646d5b9696eb996b174d8e8ae11a37d9
awesomeo184 <awesomeo184@gmail.com>
2021-02-18 23:52:18 +0900

    과제 1

--- src/main/java/com/example/servlet_tutorial/CharacterEncodingFilter.java
+++ src/main/java/com/example/servlet_tutorial/CharacterEncodingFilter.java
@@ -16,6 +16,7 @@ public class CharacterEncodingFilter implements Filter {
         throws IOException, ServletException {
 
         request.setCharacterEncoding("UTF-8");
+
         chain.doFilter(request, response);
     }
 }
